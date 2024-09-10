d68e667b64875fd3a9b7ff6b0c9675e5e48d9153
Davi Figueiredo <devdeoliveira06@gmail.com>
2024-09-10 18:32:18 -0400

    medico

--- aplicacao/src/banco/MedicoBanco.java
+++ aplicacao/src/banco/MedicoBanco.java
@@ -4,10 +4,39 @@
  */
 package banco;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import trabalho.tlp.Medico;
+
 /**
  *
  * @author davi
  */
 public class MedicoBanco {
+    
+    public void adicionar(Medico medico)
+    {   
+        try
+        {
+            Connection conexao = ConectaBanco.getConnection();
+            String sql = "insert into paciente values (default, ?, ?, ?)";
+            PreparedStatement statement = conexao.prepareStatement(sql);
+            statement.setString(1, medico.getNome());
+            statement.setInt(2, medico.getNum_crm());
+            statement.setString(3, medico.getEspecialidade());
+            ResultSet rs = statement.executeQuery();
+            
+            System.out.println("Médico adicionado com sucesso");
+            statement.close();
+            conexao.close();
+
+
+        }
+        catch(Exception e)
+        {
+        e.printStackTrace();
+        }
+    }
     
 }
--- aplicacao/src/trabalho/tlp/Medico.java
+++ aplicacao/src/trabalho/tlp/Medico.java
@@ -13,6 +13,39 @@ public class Medico {
     private String nome;
     private int num_crm;
     private String especialidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNum_crm() {
+        return num_crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNum_crm(int num_crm) {
+        this.num_crm = num_crm;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+    
     
  
     
